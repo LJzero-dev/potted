eaef38b372395f6d058fa01713bf09fc6d9cbcef
solmi04 <solmi2013@gmail.com>
2023-07-25 18:58:27 +0900

    2023-07-25 productList 정상

--- potted/src/main/java/dao/ProductListDao.java
+++ potted/src/main/java/dao/ProductListDao.java
@@ -17,14 +17,15 @@ public class ProductListDao {
 	
 	public List<ProductInfo> getProductList(int cpage, int psize, String where, String orderBy) {	// 검색조건 들어갈 곳
 		// 지정한 제품들의 목록을 List<ProductInfo>로 리턴하는 메소드
-		String sql = "select * from t_product_info a, t_product_ctgr_big b, t_product_ctgr_small c, t_product_stock d where a.pcs_id = c.pcs_id and b.pcb_id = c.pcb_id and a.pi_id = d.pi_id and a.pi_isview = 'y' and a.pi_status = 'a'" + where + " group by a.pi_id " + orderBy + " limit " + ((cpage - 1) * psize) + ", " + psize;
+		String sql = "select * from t_product_info a, t_product_ctgr_big b, t_product_ctgr_small c where a.pcs_id = c.pcs_id and b.pcb_id = c.pcb_id and a.pi_isview = 'y' and a.pi_status = 'a'" + where + " group by a.pi_id " + orderBy + " limit " + ((cpage - 1) * psize) + ", " + psize;
+		System.out.println(sql);
 		List<ProductInfo> productList = jdbc.query(sql, 
 			(ResultSet rs, int rowNum) -> {
 			ProductInfo pi = new ProductInfo(rs.getString("pi_id"), rs.getString("pcb_id"), rs.getString("pcs_id"), rs.getString("pi_name"), 
 			rs.getString("pi_status"), rs.getString("pi_img1"), rs.getString("pi_img2"), rs.getString("pi_img3"), rs.getString("pi_desc"), 
 			rs.getString("pi_special"), rs.getString("pi_isview"), rs.getString("pi_date"), rs.getString("pi_last"), rs.getInt("pi_price"), 
 			rs.getInt("pi_cost"), rs.getInt("pi_read"), rs.getInt("pi_review"), rs.getInt("pi_sale"), rs.getInt("ai_idx"), rs.getInt("pi_admin"), 
-			rs.getDouble("pi_dc"), rs.getString("pcb_name"), rs.getString("pcs_name"), rs.getInt("ps_stock"));
+			rs.getDouble("pi_dc"), rs.getString("pcb_name"), rs.getString("pcs_name"));
 			return pi;
 		});
 		
@@ -47,7 +48,7 @@ public class ProductListDao {
 							rs.getString("pi_status"), rs.getString("pi_img1"), rs.getString("pi_img2"), rs.getString("pi_img3"), rs.getString("pi_desc"), 
 							rs.getString("pi_special"), rs.getString("pi_isview"), rs.getString("pi_date"), rs.getString("pi_last"), rs.getInt("pi_price"), 
 							rs.getInt("pi_cost"), rs.getInt("pi_read"), rs.getInt("pi_review"), rs.getInt("pi_sale"), rs.getInt("ai_idx"), rs.getInt("pi_admin"), 
-							rs.getDouble("pi_dc"), rs.getString("pcb_name"), rs.getString("pcs_name"), rs.getInt("ps_stock"));
+							rs.getDouble("pi_dc"), rs.getString("pcb_name"), rs.getString("pcs_name"));
 					return pi;
 				}
 			}
